ea43fd36bed8cf5d5dd0ef3869deab804dac392f
yinheli <me@yinheli.com>
2021-07-24 14:00:03 +0800

    feat swaggerui support muti embed fs

--- pkg/swaggerui/swaggerui.go
+++ pkg/swaggerui/swaggerui.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/xinpianchang/xservice/pkg/log"
+	"go.uber.org/zap"
 )
 
 var (
@@ -22,7 +24,7 @@ const (
 )
 
 // Serve create swaggerFS middleware for serve swagger UI
-func Serve(prefix string, swaggerFs embed.FS) echo.MiddlewareFunc {
+func Serve(prefix string, efs ...embed.FS) echo.MiddlewareFunc {
 
 	tp := template.Must(template.New("index").Parse(ui))
 	var index bytes.Buffer
@@ -30,27 +32,44 @@ func Serve(prefix string, swaggerFs embed.FS) echo.MiddlewareFunc {
 		"base": base,
 	})
 
-	files := make([]map[string]string, 0, 32)
-	fs.WalkDir(swaggerFs, ".", func(src string, f fs.DirEntry, err error) error {
-		if f.IsDir() {
-			return nil
-		}
-		if !strings.HasSuffix(src, ".json") {
-			return nil
-		}
-		files = append(files, map[string]string{
-			"name": strings.TrimSuffix(src, ".swagger.json"),
-			"url":  path.Join(prefix, src),
-		})
-		return nil
-	})
+	files := make([]map[string]string, 0, 128)
+	fsm := make(map[string]int, 128)
+	hs := make(map[int]http.Handler, len(efs))
 
-	h := http.StripPrefix(prefix, http.FileServer(http.FS(swaggerFs)))
+	for i, f := range efs {
+		fs.WalkDir(f, ".", func(src string, f fs.DirEntry, err error) error {
+			if f.IsDir() {
+				return nil
+			}
+			if !strings.HasSuffix(src, ".json") {
+				return nil
+			}
+
+			name := strings.TrimSuffix(src, ".swagger.json")
+			url := path.Join(prefix, src)
+
+			if _, ok := fsm[url]; ok {
+				log.Warn("file already exists, will be ignored", zap.String("file", src))
+				return nil
+			}
+
+			fsm[url] = i
+
+			files = append(files, map[string]string{
+				"name": name,
+				"url":  url,
+			})
+
+			return nil
+		})
+
+		hs[i] = http.StripPrefix(prefix, http.FileServer(http.FS(f)))
+	}
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			requestPath := strings.TrimPrefix(c.Request().URL.Path, prefix)
-			switch requestPath {
+			path := c.Request().URL.Path
+			switch strings.TrimPrefix(path, prefix) {
 			case "", "index.html":
 				tp.Execute(c.Response().Writer, map[string]interface{}{
 					"base":  base,
@@ -58,9 +77,11 @@ func Serve(prefix string, swaggerFs embed.FS) echo.MiddlewareFunc {
 				})
 				return nil
 			default:
-				if strings.HasSuffix(requestPath, ".json") {
-					h.ServeHTTP(c.Response(), c.Request())
-					return nil
+				if strings.HasSuffix(path, ".json") {
+					if i, ok := fsm[path]; ok {
+						hs[i].ServeHTTP(c.Response(), c.Request())
+						return nil
+					}
 				}
 			}
 
